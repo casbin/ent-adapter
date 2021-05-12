f96910fdc57899cdef0cb8c99fceeddd02409466
closetool <4closetool3@gmail.com>
2021-05-12 12:21:10 +0800

    fix: lack of lisence header
    Signed-off-by: closetool <4closetool3@gmail.com>

--- adapter_test.go
+++ adapter_test.go
@@ -1,3 +1,17 @@
+// Copyright 2021 The casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package entadapter
 
 import (
@@ -241,7 +255,6 @@ func TestAdapters(t *testing.T) {
 	a = initAdapterWithClientInstance(t, db)
 	testAutoSave(t, a)
 	testSaveLoad(t, a)
-
 
 	db, err = ent.Open("postgres", "user=postgres password=postgres host=127.0.0.1 port=5432 sslmode=disable dbname=casbin")
 	if err != nil {
